86e09cfd7d001a2976f969cd94a9805183809071
Guntur Budi <guntur.budi19@gmail.com>
2018-11-05 13:03:18 +0700

    First Initial

--- TestGit/ViewController.swift
+++ TestGit/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func sum(a: Int, b: Int) -> Int{
+        return a + b
+    }
 
 }
 
