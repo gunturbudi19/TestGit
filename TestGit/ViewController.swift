f2664120528cf51a78453cf4648b2604bd904d0e
Guntur Budi <guntur.budi19@gmail.com>
2018-11-05 13:00:41 +0700

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Guntur Budi on 05/11/18.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
